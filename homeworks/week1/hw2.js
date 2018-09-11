b338c147ebd79d97f3c572d7d7375a85554d73c4
peiyunchu <r2d20328@gmail.com>
2018-09-11 18:48:52 +0800

    hw2fixed

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,20 +1,18 @@
 /*
+/*
 英文字串裡第一個字母在陣列裡是第0組
 假如第一個字母是大寫 , 就回傳大寫
 假如第一個字母是小寫 , 也回傳大寫
+假如第一個不是字母 , 則不受影響
 
 */
 
-
-function capitalize(str) {
-
-    var firstAlphabet = str[0]
-    if (firstAlphabet >= 'A' && firstAlphabet <= 'Z') 
-    return (firstAlphabet >= 'A' && firstAlphabet <= 'Z')
-    else return (firstAlphabet >= 'A' && firstAlphabet <= 'Z')
+function capitalize(str){
+    var first = str[0].toUpperCase() //利用str[0]當成第一個字母
+    return first + str.slice(1) // 第一個字母加上之後都不用變動的字元
 
 }
 
-capitalize('nick')
-
-
+console.log(capitalize('patty'))
+console.log(capitalize('nick'))
+console.log(capitalize(',hello'))
